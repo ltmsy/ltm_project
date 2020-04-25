dca3b69e306c8d94dd832118e67434459416660a
XXX <424031810@qq.com>
2020-04-25 19:02:09 +0800

    first time

--- eruka_server/src/main/java/ltm/demo/ErukaServerApplication.java
+++ eruka_server/src/main/java/ltm/demo/ErukaServerApplication.java
@@ -1,9 +1,11 @@
-package com.example.eruka_server;
+package ltm.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ErukaServerApplication {
 
     public static void main(String[] args) {
--- eruka_server/src/test/java/com/demo1/ErukaServerApplicationTests.java
+++ eruka_server/src/test/java/com/demo1/ErukaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eruka_server;
+package com.demo1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
